c166a9963ea7c0b5da9f97e388bd60481365112d
Jackie Yuan <32397155+syeehyn@users.noreply.github.com>
2021-11-02 22:28:53 -0400

    fix generalize form

--- apulu/data_pipeline/sources/news.py
+++ apulu/data_pipeline/sources/news.py
@@ -5,7 +5,7 @@ class NewsFetcher(DataFetcher):
     def __init__(self, **configs):
         super().__init__(**configs)
 
-    def get_data(self, start, end, sleep_time):
+    def get_data(self, start, end, sleep_time=None):
         """get raw data from news API
         Args:
             symbol (list): ticker symbols of stocks
