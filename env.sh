f90100f792dc99c6da4d0e719982b69a41c17de0
Jackie Yuan <sy2938@columbia.edu>
2021-11-11 17:17:45 -0500

    change env

---
+++ env.sh
@@ -0,0 +1,7 @@
+CUDA="cpu"
+TORCH_VERSION="1.9.0"
+pip install -e . &&
+pip install torch==$TORCH_VERSION -f https://download.pytorch.org/whl/$CUDA/torch_stable.html &&
+pip install torch-scatter -f https://data.pyg.org/whl/torch-$TORCH_VERSION+$CUDA.html &&
+pip install torch-sparse -f https://data.pyg.org/whl/torch-$TORCH_VERSION+$CUDA.html &&
+pip install torch-geometric
--- setup.py
+++ setup.py
@@ -9,40 +9,6 @@ from setuptools.command.install import install
 
 with open("./requirements.txt") as f:
     REQUIRED = f.read().splitlines()
-# torch version detection
-CUDA = "cpu"
-TORCH_VERSION = ""
-TORCH_EXISTS = False
-for item in REQUIRED:
-    if item[:5] == "torch":
-        TORCH_VERSION = item[5:].strip("==")
-        TORCH_EXISTS = True
-if TORCH_VERSION:
-    pass
-elif not TORCH_VERSION and TORCH_EXISTS:
-    REQUIRED.remove("torch")
-    TORCH_VERSION = "1.9.0"  # default latest
-    REQUIRED.append(f"torch=={TORCH_VERSION}")
-else:
-    TORCH_VERSION = "1.9.0"  # default latest
-    REQUIRED.append(f"torch=={TORCH_VERSION}")
-
-# TODO torch geometric dependency
-# class PostInstallCommand(install):
-#     """Post-installation for installation mode."""
-
-#     def run(self):
-#         install.run(self)
-#         # PUT YOUR POST-INSTALL SCRIPT HERE or CALL A FUNCTION
-
-#     torch_geometrics = [
-#         "torch-scatter",
-#         "torch-sparse",
-#         "torch-geometric",
-#         f"-f https://data.pyg.org/whl/torch-{TORCH_VERSION}+{CUDA}.html",
-#     ]
-#     subprocess.check_call([sys.executable, "-m", "pip", "install", *torch_geometrics])
-
 
 # build package
 setup(
