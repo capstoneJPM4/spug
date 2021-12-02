ef93e0f3d5d93dffbd17fc0ceb1651f8cc40041a
Jackie Yuan <sy2938@columbia.edu>
2021-12-01 19:16:35 -0500

    change setup point

--- env.sh
+++ env.sh
@@ -1,6 +1,6 @@
 CUDA="cpu"
 TORCH_VERSION="1.9.0"
-pip install -e . &&
+pip install -r requirements.txt
 pip install torch==$TORCH_VERSION -f https://download.pytorch.org/whl/$CUDA/torch_stable.html &&
 pip install torch-scatter -f https://data.pyg.org/whl/torch-$TORCH_VERSION+$CUDA.html &&
 pip install torch-sparse -f https://data.pyg.org/whl/torch-$TORCH_VERSION+$CUDA.html &&
