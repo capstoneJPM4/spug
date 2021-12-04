ccc98089c069be32be5eb66d35d550dc44a88f1c
Jackie Yuan <32397155+syeehyn@users.noreply.github.com>
2021-12-04 00:19:35 -0500

    Update env.sh

--- env.sh
+++ env.sh
@@ -4,5 +4,5 @@ pip install -r requirements.txt
 pip install torch==$TORCH_VERSION -f https://download.pytorch.org/whl/$CUDA/torch_stable.html &&
 pip install torch-scatter -f https://data.pyg.org/whl/torch-$TORCH_VERSION+$CUDA.html &&
 pip install torch-sparse -f https://data.pyg.org/whl/torch-$TORCH_VERSION+$CUDA.html &&
-pip install torch-geometric
+pip install torch-geometric &&
 pip install torch-geometric-temporal
